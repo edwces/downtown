b174f9b67f94d6bea7d941f8931c484d88735af6
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-04-02 15:22:06 +0200

    Check if price is a positive number in db

--- api/src/db/entities/product/product.entity.ts
+++ api/src/db/entities/product/product.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core';
+import {
+  Check,
+  Entity,
+  ManyToOne,
+  PrimaryKey,
+  Property,
+} from '@mikro-orm/core';
 import { CustomBaseEntity } from '../common/base.entity';
 import { ProductCategory } from './product-category.entity';
 
@@ -6,6 +12,7 @@ import { ProductCategory } from './product-category.entity';
 // check if price is non negative
 
 @Entity()
+@Check({ expression: 'price > 0' })
 export class Product extends CustomBaseEntity {
   @PrimaryKey()
   id!: number;
