026f1660e9c6c283ea77ae2335e559dfc0ba6608
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-04-09 13:55:15 +0200

    product module refactor

--- api/src/modules/product/product.api.ts
+++ api/src/modules/product/product.api.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import safeThrow from '../../util/safeThrow';
 import {
   createProduct,
   deleteProductById,
@@ -10,9 +11,9 @@ import {
 const product = Router();
 
 product.get('/', getProduct);
-product.get('/:id', getProductById);
+product.get('/:id', safeThrow(getProductById));
 product.post('/', createProduct);
-product.delete('/:id', deleteProductById);
-product.put('/:id', updateProductById);
+product.delete('/:id', safeThrow(deleteProductById));
+product.put('/:id', safeThrow(updateProductById));
 
 export default product;
--- api/src/modules/product/product.controller.ts
+++ api/src/modules/product/product.controller.ts
@@ -1,101 +1,61 @@
 import { wrap } from '@mikro-orm/core';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { Product } from '../../db/entities/product/product.entity';
-import ResponseError from '../../errors/response-error';
 import { HTTP_STATUS } from '../../types/enums';
+import productService from './product.service';
 
 export const getProduct = async (request: Request, response: Response) => {
-  // get query string sort_by
-  // get query string order
-  // if both are not undefined together parse orderBy into object
-  // get All products from product table
-  // return them using json format
-  const sortElement = request.query.sort_by;
-  const order = request.query.order;
+  const sortColumn = request.query.sort_by;
+  const orderDirection = request.query.order;
 
   let orderBy = {};
-  if (sortElement && order) {
-    orderBy = { [String(sortElement)]: order };
+  if (sortColumn && orderDirection) {
+    orderBy = { [String(sortColumn)]: orderDirection };
   }
+  const products = await request.em.find(Product, {}, { orderBy });
 
-  const productArray = await request.em.find(Product, {}, { orderBy });
-  response.json(productArray);
+  response.json(products);
 };
 
-export const getProductById = async (
-  request: Request,
-  response: Response,
-  next: NextFunction
-) => {
-  // get id params as integer
-  // find that product using id param
-  // catch error if it has not been found and return NotFound
-  // return product
-  const productId = Number.parseInt(request.params.id!);
-  const productFound = await request.em.findOne(Product, productId);
-  if (!productFound)
-    return next(new ResponseError('Product not Found', HTTP_STATUS.NOT_FOUND));
+export const getProductById = async (request: Request, response: Response) => {
+  const id = Number.parseInt(request.params.id!);
 
-  response.json(productFound);
+  const product = await productService.getProductById(request.em, id);
+
+  response.json(product);
 };
 
 export const createProduct = async (request: Request, response: Response) => {
-  // get validated data
-  // create new product using validated data
-  // persist it in a database and flush the commit
-  // return created product
-  const productDTO = request.body;
-  const newProduct = request.em.create(Product, productDTO);
+  const productData = request.body;
 
-  await request.em.persistAndFlush(newProduct);
+  const product = request.em.create(Product, productData);
+  await request.em.persistAndFlush(product);
 
-  response.status(HTTP_STATUS.CREATED).json(newProduct);
+  response.status(HTTP_STATUS.CREATED).json(product);
 };
 
 export const deleteProductById = async (
   request: Request,
-  response: Response,
-  next: NextFunction
+  response: Response
 ) => {
-  // get id param
-  // find entity by id
-  // catch error if it has not been found and return NotFound
-  // delete that product using found reference
-  // flush changes to database
-  // return true
+  const id = Number.parseInt(request.params.id!);
 
-  const productId = Number.parseInt(request.params.id!);
-  const productReference = await request.em.findOne(Product, productId);
-  if (!productReference)
-    return next(new ResponseError('Product not Found', HTTP_STATUS.NOT_FOUND));
+  const product = await productService.getProductById(request.em, id);
+  await request.em.removeAndFlush(product);
 
-  await request.em.remove(productReference).flush();
   response.json({ succes: true });
 };
 
 export const updateProductById = async (
   request: Request,
-  response: Response,
-  next: NextFunction
+  response: Response
 ) => {
-  // Get param id as integer
-  // Get product new data
-  // Find entity by id
-  // catch error if entity not found and return NOTFOUND
-  // wrap entity in a helper
-  // update entity fields
-  // flush commit to database
-  // retrurn entity
+  const id = Number.parseInt(request.params.id!);
+  const productData = request.body;
 
-  const productId = Number.parseInt(request.params.id!);
-  const productUpdateDTO = request.body;
+  const product = await productService.getProductById(request.em, id);
+  wrap(product).assign(productData);
+  await request.em.flush();
 
-  const productReference = await request.em.findOne(Product, productId);
-  if (!productReference)
-    return next(new ResponseError('Product not Found', HTTP_STATUS.NOT_FOUND));
-
-  const updatedProduct = wrap(productReference).assign(productUpdateDTO);
-  await request.em.persistAndFlush(updatedProduct);
-
-  response.json(updatedProduct);
+  response.json(product);
 };
