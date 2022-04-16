83accc442dbfcd1cbc23895ba4f6031fb9fdfed5
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-04-16 11:34:25 +0200

    basic navigation to register,login pages

--- web/src/modules/layout/DefaultHeader.tsx
+++ web/src/modules/layout/DefaultHeader.tsx
@@ -1,27 +1,41 @@
-import { ActionIcon, Group, Header, Title } from '@mantine/core';
+import { ActionIcon, Group, Header, Menu, Title } from '@mantine/core';
+import { NextLink } from '@mantine/next';
 import Link from 'next/link';
-import { useState } from 'react';
-import { ShoppingCart } from 'tabler-icons-react';
+import { ShoppingCart, User } from 'tabler-icons-react';
 
 export default function DefaultHeader() {
   return (
     <Header
       height={60}
-      sx={(theme) => ({
+      sx={{
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
-      })}
+      }}
     >
       <Group spacing="sm" pl="md">
         <Title>Downtown</Title>
       </Group>
       <Group spacing="sm" pr="md">
         <Link href="/checkout" passHref>
-          <ActionIcon size="xl" variant="filled">
+          <ActionIcon size="xl" variant="filled" component="a">
             <ShoppingCart />
           </ActionIcon>
         </Link>
+        <Menu
+          control={
+            <ActionIcon size="xl" variant="filled">
+              <User />
+            </ActionIcon>
+          }
+        >
+          <Menu.Item component={NextLink} href="/user/register">
+            Register
+          </Menu.Item>
+          <Menu.Item component={NextLink} href="/user/login">
+            Login
+          </Menu.Item>
+        </Menu>
       </Group>
     </Header>
   );
