86d4ca19bba11bfb77dfbc66a9f93e604ed5733f
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-09-09 09:19:06 +0200

    setup axios and react query

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "../config/react-query.config";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
